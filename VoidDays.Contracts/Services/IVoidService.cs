094884a359be44e4018108832a63f0463ef77c48
msradtke <mick.radtke@gmail.com>
2019-05-19 08:33:21 -0500

    vd

---
+++ VoidDays.Contracts/Services/IVoidService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidDays.Contracts.Services
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface IVoidService
+    {
+        [OperationContract]
+        bool CreateUser(string username, string password);
+        [OperationContract]
+        bool Login(string username, string password);
+        [OperationContract]
+        bool ChangePassword(string username, string password, string newPassword);
+
+    }
+}
--- VoidDays.Services/VoidService.cs
+++ VoidDays.Services/VoidService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoidDays.Contracts.Services;
 
 namespace VoidDays.Services
 {
-    public class Class1
+    public class VoidService : IVoidService
     {
+        
     }
 }
