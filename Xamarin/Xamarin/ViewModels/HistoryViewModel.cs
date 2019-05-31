b7f37cf3944f40b9f854fb305eacf8973f72952a
Mick Radtke <mick.radtke@gmail.com>
2019-05-31 16:00:19 -0500

    mobile

---
+++ Xamarin/Xamarin/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Navigation;
+
+namespace Xamarin.ViewModels
+{
+    public class HistoryViewModel : ViewModelBase
+    {
+        public HistoryViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+
+        public override void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            base.OnNavigatedFrom(parameters);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+        }
+
+        public override void OnNavigatingTo(INavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+        }
+    }
+}
